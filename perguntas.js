74a52f65f8058e4d83d61f384656f6d360589919
Gustavo H <159548979+morkzzz@users.noreply.github.com>
2024-09-12 11:49:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'LoL',
+    'O que é late game?',
+    'É uma tag que significa o final do jogo'
+)
+
+criaCartao(
+    'COD',
+    'O que é bait?',
+    'Uma tag para armadilha no jogo COD'
+)
+
+criaCartao(
+    'Free Fire',
+    'O que significa HS?',
+    'Significa tiro na cabeça'
+)
+
+criaCartao(
+    'Brawl Stars',
+    'O que é um brawler?',
+    'É um nome para personagem'
+)
